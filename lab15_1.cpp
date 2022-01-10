bb4f60242da4b11be04ce1eab958f2cbbf030b70
M <thanaporn_chan@cmu.ac.th>
2022-01-10 20:04:00 +0700

    15_1 complete

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -2,18 +2,38 @@
 using namespace std;
 
 template <typename T>
-void insertionSort(T d[],int N){
-
+void insertionSort(T d[], int N)
+{
+	T temp;
+	for (int i = 0; i < N - 1; i++)
+	{
+		cout << "Pass " << i+1 << ":";
+		for (int j = i; j > -1; j--)
+		{	
+			temp = d[j+1];
+			if (temp > d[j])
+			{
+				d[j+1] = d[j];
+				d[j] = temp;
+			}
+		}
+		for(int k = 0; k < N; k++)
+				cout << d[k] << " ";
+		cout << "\n";
+	}
 }
 
-int main(){
-	int a[10] = {12,25,30,44,2,0,4,7,55,25};
+int main()
+{
+	int a[10] = {12, 25, 30, 44, 2, 0, 4, 7, 55, 25};
 	cout << "Input Array:";
-	for(int i = 0; i < 10; i++) cout << a[i] << " ";
+	for (int i = 0; i < 10; i++)
+		cout << a[i] << " ";
 	cout << "\n\n";
-	
-	insertionSort(a,10);
-	
+
+	insertionSort(a, 10);
+
 	cout << "\nSorted Array:";
-	for(int i = 0; i < 10; i++) cout << a[i] << " ";	
+	for (int i = 0; i < 10; i++)
+		cout << a[i] << " ";
 }
